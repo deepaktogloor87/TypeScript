687c81c0973e580692aef521effd53f56fef6207
Deepak Togloor <deepaktogloor87@gmail.com>
2025-01-31 10:06:41 +0530

    first program

---
+++ Topic_1/app.js
@@ -0,0 +1,7 @@
+// how write a small program
+var num = 10;
+console.log(num);
+// how to run ts file
+// To run the ts file we need write command tsc filename.ts
+// how to generate js file
+// how to view on browser
---
+++ Topic_1/app.ts
@@ -0,0 +1,11 @@
+// how write a small program
+
+const num:number = 10;
+console.log(num);
+
+
+// To run the ts file we need write command tsc filename.ts
+// tsc app.ts, this will complie and generate app.js file like this : C:\Users\Deepak\Desktop\TypeScript\Topic_1\app.js
+
+// how to view on browser
+// crate index.html file, src=app.js in script block in html, open the browser --> goto console tab to see the result.
