0dbf5a5b175b370ff8b2c4dd7574c1868805be2e
Deepak Togloor <deepaktogloor87@gmail.com>
2025-01-31 12:13:26 +0530

    object type

---
+++ Topic_5/object.js
@@ -0,0 +1,24 @@
+var person = {
+    name: "deepak",
+    age: 20,
+    email: "deepaktogloor@gmail.com"
+};
+console.warn(person.email);
+console.warn(person.name);
+console.warn(person.age);
+var user = {
+    name: "deepak",
+    age: 20,
+    email: "deepaktogloor@gmail.com"
+};
+console.warn(user.email);
+console.warn(user.name);
+console.warn(user.age);
+var new_user = {
+    name: "advik",
+    age: 10,
+    email: "dt@gmail.com"
+};
+console.warn(new_user.email);
+console.warn(new_user.name);
+console.warn(new_user.age);
