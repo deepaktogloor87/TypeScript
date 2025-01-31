67506f2e218bc80efbf222cafd6c68374ccaf741
Deepak Togloor <deepaktogloor87@gmail.com>
2025-01-31 11:10:43 +0530

    core types

---
+++ Topic_2/coredatatypes.js
@@ -0,0 +1,19 @@
+// core types
+// number, string, boolean
+// integer, floating can be defined in number only
+// string can be defined in single, double quotes
+// boolean can be defined in true, false
+// let num:number=10;
+// let str:string="hello";
+// let bool:boolean=true;
+// function add(n1:number,n2:number)
+// {
+//     return n1+n2
+// }
+// let result = add(10,20);
+// console.log(result);
+function dummy(a) {
+    return a;
+}
+var result = dummy("hello typescript");
+console.log(result);
