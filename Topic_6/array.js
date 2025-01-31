4955d00c941ebc9c052fac59aa86e29c1551457d
Deepak Togloor <deepaktogloor87@gmail.com>
2025-01-31 12:21:57 +0530

    arrays

---
+++ Topic_6/array.js
@@ -0,0 +1,7 @@
+// array types in typescript
+// array is basically collection of values
+var arr = ['php', 'js', 'ts', 'node'];
+arr[2] = '123';
+arr.push("hello");
+console.log(arr);
+// let random:any[]=['php','js','ts','node',1,2,3,4,5,6,7,8,9,10]
